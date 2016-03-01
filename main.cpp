01d54cbee64548d33f7b9b346bf12c4343578b64
Mukhad <mukhad@yandex.ru>
2016-03-01 14:03:38 +0300

    With daemonization v0.3a

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 
 //sudo apt-get install libev-dev
-//https://github.com/mukhad/serverOneEmpty.git
+//echo https://github.com/mukhad/serverOneEmpty.git > final.txt
 
 using namespace std;
 
@@ -23,49 +23,56 @@ int main(int argc, char *argv[])
 
     int forkpid = 0;
 
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
+
 
     if(start_demon) forkpid=fork();
     if(!forkpid)
     {
-        FILE *flog;
-        flog = fopen ("server_final.log" , "a");
-        if (flog == NULL){ printf ("Error opening file"); return 0;}
-        char cmd1[]="Start";
-        fprintfTimeToLogFile(flog,cmd1);
+        //FILE *flog;
+        //flog = fopen ("server_final.log" , "a");
+        //if (flog == NULL){ printf ("Error opening file"); return 0;}
+        //char cmd1[]="Start";
+        //fprintfTimeToLogFile(flog,cmd1);
 
-        cout << "demonization!!! pid:"<< getpid() << endl;
-        for (int i=0;i<argc;i++){
-                printf("Argument %d: %s\n",i,argv[i]);
-        }
+        //cout << "demonization!!! pid:"<< getpid() << endl;
+        //for (int i=0;i<argc;i++){
+        //        printf("Argument %d: %s\n",i,argv[i]);
+        //}
 
 
-        cout << "server: " << ip << ":"<< iport << "/" << directory << "  mode:" << mode << endl;
-        fprintf(flog,"server:  %s:%s/%s\n",ip,port,directory);
+        //cout << "server: " << ip << ":"<< iport << "/" << directory << "  mode:" << mode << endl;
+        //fprintf(flog,"server:  %s:%s/%s\n",ip,port,directory);
 
         // создаём новый сеанс, чтобы не зависеть от родителя и переходим в корень диска
         if(start_demon){
             setsid();
-            chdir("/");
+
+            signal(SIGCHLD, SIG_IGN);
+            signal(SIGHUP, SIG_IGN);
+
             umask(0);
-            close(STDIN_FILENO);    close(STDOUT_FILENO);    close(STDERR_FILENO);
+            chdir("/");
+
+            /* Close all open file descriptors */
+            //close(STDIN_FILENO);    close(STDOUT_FILENO);    close(STDERR_FILENO);
+            for (int x = sysconf(_SC_OPEN_MAX); x>0; x--)   close (x);
+
         }
 
 
         startEvLoop(iport,directory);
 
-        char cmd2[]="End";
-        fprintfTimeToLogFile(flog,cmd2);
+        //char cmd2[]="End";
+        //fprintfTimeToLogFile(flog,cmd2);
 
-        fclose (flog);
+        //fclose (flog);
         //sleep(15);
     }
 
 
 
-    int pid = getpid();
-    cout << "stop, pid:"<< pid << endl;
+    //int pid = getpid();
+    //cout << "stop, pid:"<< pid << endl;
 
 
     return 0;
